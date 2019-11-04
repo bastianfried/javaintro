02d06a2d957b209f4324dfdd566458ea3125b264
bastian.fried <bastian.fried@AMS-GYM-EDV1-03.gym.ams.local>
2019-11-04 18:16:38 +0100

    Textausgabe; Einfaches Rechnen

--- src/javaBasics/JavaBasics.java
+++ src/javaBasics/JavaBasics.java
@@ -3,8 +3,40 @@ package javaBasics;
 public class JavaBasics {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Text");
+		
+		//Kommentar Text einzeillig	
+		
+		/* mehrzeiliger
+		 * Kommentar
+		 */
+		
+		//Unterschied zwichen print und println
+		System.out.print("Hallo");
+		System.out.print(" ");
+		System.out.print("du");
+		System.out.print(" ");
+		System.out.println("!");
+		//
+		//Textverkettung
+		System.out.println("Hallo"+" "+"du"+" "+"!");
+		//
+		System.out.println("Hallo du !");
+		//
+		System.out.println(120);
+		System.out.println(1);
+		System.out.println(2);
+		//
+		System.out.println(123.456);
+		//
+		//System.out.println(123,456);
+		//
+		System.out.println(2+2);
+		System.out.println(4-3);
+		System.out.println(3*3);
+		System.out.println(9/3);
+		//
 	}
+	
 
 }
