9c0ee485ecd59b668256c86bcaef7abc34718258
bastian.fried <bastian.fried@AMS-GYM-EDV1-03.gym.ams.local>
2019-11-11 18:48:25 +0100

    EinMalEins + Schleifen

---
+++ src/javaBasics/EinMalEins.java
@@ -0,0 +1,21 @@
+package javaBasics;
+
+public class EinMalEins {
+
+	public static void main(String[] args) {
+		for (int i=1;i<11;i = i +1 ) {
+			for(int j=1;j<11;j = j+1){
+			System.out.println(j+"x"+i+"="+ (i*j));
+			
+						
+							
+						}
+					
+				}
+				
+			}
+		
+		}
+	
+
+
--- src/javaBasics/Schleifenbasics.java
+++ src/javaBasics/Schleifenbasics.java
@@ -8,44 +8,25 @@ public class Schleifenbasics {
 		for (int i = 0; i < 5; i= i+1)
 			System.out.println(i);
 			
+		//
+		System.out.println("-----");
+		//
+		int j =1;
+		for (;j<10;) {
+			System.out.println(j);
+			j = j +1;	
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		//endlosschleife
+		//for (;3<4;) {
+			//System.out.println("HueHue");
+		//}
+		}
 		
 		
 		
 		
 	}
+	
+	
 
 }
