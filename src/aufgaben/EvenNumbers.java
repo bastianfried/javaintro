a5a9ff0aa92cefc8354527047d4e54f82036d543
bastian.fried <bastian.fried@AMS-GYM-EDV1-03.gym.ams.local>
2019-11-18 18:15:45 +0100

    evennumbers

---
+++ src/aufgaben/EvenNumbers.java
@@ -0,0 +1,14 @@
+package aufgaben;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		
+		for (int x = 1; x<=100; x = x + 1) {
+			if (x % 2 == 0)
+				System.out.println(x);
+		}
+
+	}
+
+}
