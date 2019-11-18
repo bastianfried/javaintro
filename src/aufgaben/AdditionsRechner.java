d6ef781920b8db591cbbd719f2bb9236b2a4971b
bastian.fried <bastian.fried@AMS-GYM-EDV1-03.gym.ams.local>
2019-11-18 18:41:33 +0100

    damit mein pc nicht umgebracht wird

--- src/aufgaben/AdditionsRechner.java
+++ src/aufgaben/AdditionsRechner.java
@@ -3,7 +3,7 @@ package aufgaben;
 public class AdditionsRechner {
 
 	public static void main(String[] args) {
-		for(int zahlA = 1; zahlA<100; zahlA=zahlA);
+		for(int zahlA = 1; zahlA<100; zahlA=zahlA + 1);
 
 	}
 
