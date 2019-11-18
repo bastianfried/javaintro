671df8bf58794ab5fa08373d8e3c3b7cdac9faed
basti <basti@DESKTOP-N04QSD5>
2019-11-18 19:39:57 +0100

    Addition

--- src/aufgaben/AdditionsRechner.java
+++ src/aufgaben/AdditionsRechner.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class AdditionsRechner {
 
 public static void main(String[] args) {
-int x,z,y;
+int x,y,z;
 Scanner eingabe = new Scanner(System.in);
 //
-        System.out.print(" ");
+        System.out.print("");
         x = eingabe.nextInt();
-        System.out.print(" ");
+        System.out.print("");
         y = eingabe.nextInt();
         z = x + y;
         System.out.println( z );
