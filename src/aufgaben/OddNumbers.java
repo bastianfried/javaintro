faaf827b01c4dcde8c669def6a7a78c0a90a0a11
bastian.fried <bastian.fried@AMS-GYM-EDV1-03.gym.ams.local>
2019-11-18 18:14:12 +0100

    mit smart art

--- src/aufgaben/OddNumbers.java
+++ src/aufgaben/OddNumbers.java
@@ -3,9 +3,13 @@ package aufgaben;
 public class OddNumbers {
 
 	public static void main(String[] args) {
-		for (int odd = 1;odd<=100; odd = odd + 2) {
-		System.out.println(odd);
-		}
+//		for (int odd = 1;odd<=100; odd = odd + 2) {
+//		System.out.println(odd);
+//		}
+	for (int x = 1; x<=100; x = x + 1) {
+		if (x % 2 == 1)
+			System.out.println(x);
+	}
 		
 		
 
