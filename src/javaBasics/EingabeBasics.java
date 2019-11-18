2372679be3c75418a9b7c74f3a0ac922cafc698e
bastian.fried <bastian.fried@AMS-GYM-EDV1-03.gym.ams.local>
2019-11-18 18:31:45 +0100

    xD

---
+++ src/javaBasics/EingabeBasics.java
@@ -0,0 +1,15 @@
+package javaBasics;
+
+import java.util.Scanner;
+
+public class EingabeBasics {
+
+	public static void main(String[] args) {
+		Scanner eingabe = new Scanner(System.in);
+				//
+		System.out.println(eingabe.nextLine());
+		
+
+	}
+
+}
