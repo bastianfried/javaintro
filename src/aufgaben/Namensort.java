ac3d758e7a4691807a7b982d9b1049d711a93d91
bastian.fried <bastian.fried@AMS-GYM-EDV1-03.gym.ams.local>
2020-01-27 18:51:48 +0100

    NamenSort

---
+++ src/aufgaben/Namensort.java
@@ -0,0 +1,43 @@
+package aufgaben;
+
+public class Namensort {
+                          
+public static void main(String[] args) {
+
+String name1 = "Bastian Fried";
+String name2 = "Viktor Sun";
+String name3 = "Chimerie Noble";
+String name4 = "Oleg Nikitin";
+String name5 = "David Karakash";
+String name6 = "Theodor Kondratev";
+String name7 = "Martin Medek";
+String name8 = "Leo Honfi";
+
+int diff = name1.compareTo(name2);
+if (diff>0) {
+	System.out.println(diff);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+	}
+
+}
