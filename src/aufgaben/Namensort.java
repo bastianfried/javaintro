db217683a11af51c4c5afae750d38d13c65a3066
bastian.fried <bastian.fried@AMS-GYM-EDV1-03.gym.ams.local>
2020-02-17 17:40:21 +0100

    :)

--- src/aufgaben/Namensort.java
+++ src/aufgaben/Namensort.java
@@ -7,8 +7,7 @@ public class Namensort {
 	public static void main(String[] args) {
 		int i, j;
 		String t;
-		
-		
+
 		ArrayList<String> namen = new ArrayList<String>();
 		namen.add("Bastian");
 		namen.add("Viktor");
@@ -19,24 +18,19 @@ public class Namensort {
 		namen.add("Theodor");
 		namen.add("Martin");
 		namen.add("Martina");
-	
+
 		int n = namen.size();
 		for (i = 1; i < n; i++) {
-			for (j = 0; j < (n-1); j++) {
-		
-			if (namen.get(j).compareToIgnoreCase(namen.get(j + 1)) > 0) {
-			t = namen.get(j);
-			namen.set(j,namen.get(j+1));
-			namen.set(j+1,t);
-			
-			
-			
-			
-				
-			
-			}		
-		}	
+			for (j = 0; j < (n - 1); j++) {
+
+				if (namen.get(j).compareToIgnoreCase(namen.get(j + 1)) > 0) {
+					t = namen.get(j);
+					namen.set(j, namen.get(j + 1));
+					namen.set(j + 1, t);
+
+				}
+			}
 		}
 		System.out.println(namen);
-		}
+	}
 }
