0c8386c1cee44df7884d524b5afd588c298b864e
bastian.fried <bastian.fried@AMS-GYM-EDV1-03.gym.ams.local>
2020-02-17 17:32:48 +0100

    Namensort fetich

--- src/aufgaben/Namensort.java
+++ src/aufgaben/Namensort.java
@@ -5,24 +5,38 @@ import java.util.ArrayList;
 public class Namensort {
 
 	public static void main(String[] args) {
-
+		int i, j;
+		String t;
+		
 		
 		ArrayList<String> namen = new ArrayList<String>();
-		namen.add("Bastian Fried");
-		namen.add("Viktor Sun");
-		namen.add("Chemmy Noble");
-		namen.add("Oleg Nikitin");
-		namen.add("David Karakash");
-		namen.add("Leo Honfi");
-		namen.add("Theodor Kondratev");
-		namen.add("Martin Medek");
-
-		
-		
-			
-		}
-		}
-
+		namen.add("Bastian");
+		namen.add("Viktor");
+		namen.add("Chemmy");
+		namen.add("Oleg");
+		namen.add("David");
+		namen.add("Leo");
+		namen.add("Theodor");
+		namen.add("Martin");
+		namen.add("Martina");
 	
-
-
+		int n = namen.size();
+		for (i = 1; i < n; i++) {
+			for (j = 0; j < (n-1); j++) {
+		
+			if (namen.get(j).compareToIgnoreCase(namen.get(j + 1)) > 0) {
+			t = namen.get(j);
+			namen.set(j,namen.get(j+1));
+			namen.set(j+1,t);
+			
+			
+			
+			
+			
+			
+			}		
+		}	
+		}
+		System.out.println(namen);
+		}
+}
